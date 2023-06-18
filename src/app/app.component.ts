4e4284b9d3365468c5549f079b6373764e9efcf1
aladar <annamaria.demartino@gmail.com>
2023-06-18 16:43:18 +0200

    First Release

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'sg03-typing';
+  randomText = faker.lorem.sentence({ min: 2, max: 4 });
+  isSuccessVisible = false;
+  enteredText = '';
+  className = '';
+
+  // onInput(value: string) {
+  //   this.enteredText = value;
+  //   if (this.randomText == this.enteredText) this.isSuccessVisible = true;
+  // }
+
+  onInput(value: string) {
+    this.enteredText = value;
+  }
+  compare(randomLetter: string, enteredLetter: string) {
+    if (!enteredLetter) return 'pending';
+    return randomLetter === enteredLetter ? 'correct' : 'incorrect';
+  }
 }
